32ab250874c9b8f23d23e4ccf20a9019b410b070
saurabh-gaonkar <saurabhg@axioned.com>
2022-04-12 18:28:13 +0530

    js added for video model functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,19 @@
-/* Author: 
+$(document).ready(function () {
+    $(".hamburger").click(function () {
+        $(".header-top__menus").slideToggle();
+        $(this).toggleClass("cross");
+    });
 
-*/
+    $(".play-button").click(function () {
+        $(".modal-background").show();
+        $(".page-content__taking-the-lead__play-video").show();
+    });
 
+    $(".modal-background").click(function () {
+        $(this).hide();
+    })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $(".header-top__menus ul li").click(function () {
+        $(this > ul).show();
+    })
+})
